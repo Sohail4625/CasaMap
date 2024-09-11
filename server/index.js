b76f3ccf51e43625cb1f8e03a6891f8e247bb0a5
Sohail4625 <154996218+Sohail4625@users.noreply.github.com>
2024-09-12 02:08:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,9 @@ port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log("Server is running");
 });
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 process.on("SIGINT", async () => {
   await mongoose.connection.close();
   console.log("Mongoose connection closed due to app termination");
